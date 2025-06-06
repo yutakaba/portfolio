d319226454c9867b438f7d330300cae8ef2156fb
yutakaba <lntdk1121@gmail.com>
2025-06-06 15:48:42 +0900

    ESLintの導入と設定追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,41 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...storybook.configs["flat/recommended"]
-];
-
-export default eslintConfig;
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"] 
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], 
+    languageOptions: { 
+      globals: globals.browser 
+    } 
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ["**/*.json"],
+    plugins: {
+      jsonc: pluginJsonc
+    },
+    languageOptions: {
+      parser: pluginJsonc.parsers.JSON,
+    },
+    rules: pluginJsonc.configs.recommended.rules
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
